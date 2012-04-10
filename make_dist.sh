dfa1f1a470fa8e40003e0e50ea81434a3c4c6897
Claudio Satriano <satriano@gmail.com>
2012-04-10 09:31:23 +0200

    Small change

--- make_dist.sh
+++ make_dist.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export LC_ALL=C
+TESTDATA=testdata
 
 timestamp=`date +"%Y%m%d"`
 
 tarfile=source_spec_${timestamp}.tgz
 
-tar cvfz ../$tarfile *.py README.txt testdata
+tar cvfz ../$tarfile *.py README.txt $TESTDATA
