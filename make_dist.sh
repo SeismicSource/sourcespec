4703fa8ea18233681f6b17b5ed1d4d1a27ce0e09
Claudio Satriano <satriano@gmail.com>
2013-04-10 12:06:45 +0200

    make_dist.sh better handles new directory structure

--- make_dist.sh
+++ make_dist.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 export LC_ALL=C
-TESTDATA=testdata
 
 timestamp=`date +"%Y%m%d"`
 
-tarfile=source_spec_${timestamp}.tgz
+dir=source_spec_${timestamp}
+tarfile=${dir}.tgz
 
-tar cvfz ../$tarfile *.py README.md ChangeLog.txt $TESTDATA
+mkdir $dir
+rsync -a\
+    --exclude='.*'\
+    --exclude='*.pyc'\
+    --exclude='*.sqlite'\
+    --exclude='sspec_out'\
+    --exclude='make_dist.sh'\
+    --exclude="$dir"\
+    . $dir
+tar cvfz ../$tarfile $dir
+rm -rf $dir
