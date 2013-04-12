6d7f1405711471a45bfba237b1647d76224e13ac
Claudio Satriano <satriano@gmail.com>
2013-04-12 15:18:53 +0200

    Reordering variables in spectral model

--- lib/ssp_spectral_model.py
+++ lib/ssp_spectral_model.py
@@ -12,4 +12,4 @@ def spectral_model(freq, Mw, fc, t_star):
     # log S(w)= log(coeff*Mo) + log((1/(1+(w/wc)^2)) + log (exp (- w *t_star/2)) 
     # attenuation model: exp[-pi t* f] with t*=T /Q
     loge = math.log10(math.e)
-    return Mw - np.log10(1. + np.power((freq/fc),2) ) - loge*(math.pi*t_star*freq) 
+    return Mw - np.log10(1. + np.power((freq/fc),2)) - loge*(math.pi*freq*t_star) 
