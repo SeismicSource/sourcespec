fc154dcd022c9b51b97838a5ef3c5ed492a484b1
Claudio Satriano <satriano@gmail.com>
2013-05-29 14:16:36 +0200

    Updated signal to noise computation.
    - Noise window for S/N is from trace start to P-wave arrival;
    - Noise window for weigthing is as before;
    - Signal window for S/N starts `config.pre_s_time` before S-wave
      arrival and has the same length than noise window.
    
    Thanks Agnès Chounet for these changes!
    
    Small bug fix:
    - rms for signal level is now correctly computed
      (it was before rms2)

--- lib/ssp_process_traces.py
+++ lib/ssp_process_traces.py
@@ -43,8 +43,9 @@ def process_traces(config, st):
         trace.detrend(type='constant')
         trace.detrend(type='constant')
         rms2 = np.power(trace.data, 2).sum()
+        rms = np.sqrt(rms2)
         rms_min = config.rmsmin
-        if rms2 <= rms_min:
+        if rms <= rms_min:
             logging.warning('%s: Trace RMS smaller than %g: skipping trace' % (traceId, rms_min))
             continue
 
@@ -54,21 +55,50 @@ def process_traces(config, st):
             logging.warning('%s: Unable to remove instrument response: skipping trace' % traceId)
             continue
 
-        # check if the trace has (significant) signal to noise ratio
+        # Check if the trace has (significant) signal to noise ratio
         #### start signal/noise ratio
         p_arrival_time = pwave_arrival(trace, config.vp)
         s_arrival_time = swave_arrival(trace, config.vs)
-        # S time window
-        pt1 = s_arrival_time - config.pre_s_time
-        pt2 = pt1 + config.s_win_length
+
+        # noise time window for s/n ratio
+        noise_start_t = trace.stats['starttime']
+        noise_end_t = p_arrival_time
+        noise_win_length = noise_end_t - noise_start_t
+        
+        trace_noise = copy(trace)
+        trace_noise.stats = deepcopy(trace.stats)
+        # remove the mean...
+        trace_noise.detrend(type='constant')
+        # ...and the linear trend...
+        trace_noise.detrend(type='linear')
+        trace_noise.trim(starttime=noise_start_t, endtime=noise_end_t, pad=True, fill_value=0)
+        
+        # S window for s/n ratio
+        s_start_t = s_arrival_time - config.pre_s_time
+        s_end_t = s_start_t + noise_win_length
         trace_cutS = copy(trace)
         trace_cutS.stats = deepcopy(trace.stats)
         # remove the mean...
         trace_cutS.detrend(type='constant')
         # ...and the linear trend...
         trace_cutS.detrend(type='linear')
-        trace_cutS.trim(starttime=pt1, endtime=pt2, pad=True, fill_value=0)
-        # noise time window
+        trace_cutS.trim(starttime=s_start_t, endtime=s_end_t, pad=True, fill_value=0)
+        
+        rmsnoise2 = np.power(trace_noise.data, 2).sum()
+        rmsnoise = np.sqrt(rmsnoise2)
+        rmsS2 = np.power(trace_cutS.data, 2).sum()
+        rmsS = np.sqrt(rmsS2)
+
+        sn_ratio = rmsS/rmsnoise
+        logging.info('%s.%s S/N: %.1f' % (station, trace.stats.instrtype, sn_ratio))
+
+        snratio_min = config.sn_min
+        if sn_ratio <= snratio_min:
+            logging.warning('%s: S/N smaller than %g: skipping trace' % (traceId, snratio_min))
+            continue
+        #### end signal/noise ratio
+
+        # Noise time window for weighting function:
         nt1 = p_arrival_time - config.pre_noise_time
         nt2 = nt1 + config.s_win_length
         trace_noise = copy(trace)
@@ -78,22 +108,6 @@ def process_traces(config, st):
         # ...and the linear trend...
         trace_noise.detrend(type='linear')
         trace_noise.trim(starttime=nt1, endtime=nt2, pad=True, fill_value=0)
-
-        rmsnoise2 = np.power(trace_noise.data, 2).sum()
-        rmsnoise = np.sqrt(rmsnoise2)
-        rmsP2 = np.power(trace_cutS.data, 2).sum()
-        rmsP = np.sqrt(rmsP2)
-
-        sn_ratio = rmsP/rmsnoise
-
-        snratio_min = config.sn_min
-        if sn_ratio <= snratio_min:
-            logging.warning('%s: S/N smaller than %g: skipping trace' % (traceId, snratio_min))
-            continue
-
-        evid = trace.stats.hypo.evid 
-        print '%s %s.%s %s' % (evid, station, trace.stats.instrtype, sn_ratio)
-        #### end signal/noise ratio
 
         orig_trace.stats.hypo_dist = hd
         out_st.append(trace)
