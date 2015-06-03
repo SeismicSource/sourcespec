4cc871ada998de24c5c4ce44ffb280a9cd977f58
Claudio Satriano <satriano@gmail.com>
2015-06-03 14:45:16 +0200

    Fix plotting for traces with missing data.

--- lib/ssp_plot_traces.py
+++ lib/ssp_plot_traces.py
@@ -92,6 +92,11 @@ def plot_traces(config, st, ncols=4, block=True):
             for trace in st_sel.traces:
                 if trace.stats.channel[0:2] != code:
                     continue
+                starttime = trace.stats.starttime
+                t1 = starttime + trace.stats.arrivals['P'][1] - config.pre_p_time
+                t2 = starttime + trace.stats.arrivals['S'][1] + 3 * config.s_win_length
+                trace.trim(starttime=t1, endtime=t2, pad=True, fill_value=0)
+                delta_stime = trace.stats.starttime - starttime
                 orientation = trace.stats.channel[-1]
                 if orientation in ['Z', '1']:
                     color = 'purple'
@@ -103,12 +108,6 @@ def plot_traces(config, st, ncols=4, block=True):
                     color = 'blue'
                     if ntraces > 1:
                         trace.data = (trace.data / tmax + 1) * tmax
-
-                starttime = trace.stats.starttime
-                t1 = starttime + trace.stats.arrivals['P'][1] - config.pre_p_time
-                t2 = starttime + trace.stats.arrivals['S'][1] + 3 * config.s_win_length
-                trace.trim(starttime=t1, endtime=t2, pad=True, fill_value=0)
-                delta_stime = trace.stats.starttime - starttime
                 ax.plot(trace.times(), trace, color=color, zorder=20)
                 ax.text(0.05, trace.data.mean(), trace.stats.channel, color=color,
                         transform=trans3, zorder=22)
