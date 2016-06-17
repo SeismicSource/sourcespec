61a996a51a944dc108764bba9cc8e95b7adf024a
Claudio Satriano <satriano@gmail.com>
2016-06-17 18:18:31 +0200

    Template mechanism to set Magnitude ids
    Also an option to set sourcespec magnitude as preferred magnitude in
    QuakeML.

--- sourcespec/ssp_output.py
+++ sourcespec/ssp_output.py
@@ -421,17 +421,19 @@ def _write_qml(config, sourcepar):
         origin = ev.origins[0]
     origin_id = origin.resource_id
     origin_id_strip = origin_id.id.split('/')[-1]
-    origin_id_strip = origin_id_strip.replace('Origin', '')
+    origin_id_strip = origin_id_strip.replace(
+        config.smi_strip_from_origin_id, '')
+    _id = config.smi_magnitude_template.replace('$SMI_BASE', config.smi_base)
+    _id = _id.replace('$ORIGIN_ID', origin_id_strip)
 
     means = sourcepar.means
     errors = sourcepar.errors
     stationpar = sourcepar.station_parameters
 
     mag = Magnitude()
-    _id = 'smi:scs/0.7/Magnitude' + origin_id_strip + '.sourcespec'
     mag.resource_id = ResourceIdentifier(id=_id)
     ssp_version = get_versions()['version']
-    _id = 'smi:scs/0.7/sourcespec/' + ssp_version
+    _id = config.smi_base + '/sourcespec/' + ssp_version
     mag.method_id = ResourceIdentifier(id=_id)
     mag.origin_id = origin_id
     mag.magnitude_type = 'Mw'
@@ -456,7 +458,11 @@ def _write_qml(config, sourcepar):
         par = stationpar[statId]
         st_mag = StationMagnitude()
         seed_id = statId.split()[0]
-        _id = mag.resource_id.id + '#' + seed_id
+        _id = config.smi_station_magnitude_template.replace(
+            '$SMI_MAGNITUDE_TEMPLATE', config.smi_magnitude_template)
+        _id = _id.replace('$ORIGIN_ID', origin_id_strip)
+        _id = _id.replace('$SMI_BASE', config.smi_base)
+        _id = _id.replace('$WAVEFORM_ID', seed_id)
         st_mag.resource_id = ResourceIdentifier(id=_id)
         st_mag.origin_id = origin_id
         st_mag.mag = par['Mw']
@@ -470,8 +476,8 @@ def _write_qml(config, sourcepar):
         mag.station_magnitude_contributions.append(st_mag_contrib)
     ev.magnitudes.append(mag)
 
-    # if config.set_preferred_magnitude:
-    #     ev.preferred_magnitude_id = mag.resource_id.id
+    if config.set_preferred_magnitude:
+        ev.preferred_magnitude_id = mag.resource_id.id
 
     qml_file_out = os.path.join(config.options.outdir, evid + '.xml')
     ev.write(qml_file_out, format='QUAKEML')
