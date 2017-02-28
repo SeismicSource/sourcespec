f636e16028ab2b7bf5690d08b114ab3f3efbed0d
Claudio Satriano <satriano@gmail.com>
2017-02-28 16:17:27 +0100

    ssp_output: use string.format() everywhere
    Also improves a little bit the text file output.

--- sourcespec/ssp_output.py
+++ sourcespec/ssp_output.py
@@ -44,14 +44,15 @@ def _format_exponent(value, reference):
     # get the exponent of reference value
     xp = int(np.log10(reference))
     # format value to print it with the same exponent of reference value
-    return '%5.3fe%+03d' % (value/10**xp, xp)
+    return '{:5.3f}e{:+03d}'.format(value/10**xp, xp)
 
 
 def _write_parfile(config, evid, sourcepar):
     """Write station source parameters to file."""
     if not os.path.exists(config.options.outdir):
         os.makedirs(config.options.outdir)
-    parfilename = os.path.join(config.options.outdir, '%s.ssp.out' % evid)
+    parfilename = os.path.join(
+        config.options.outdir, '{}.ssp.out'.format(evid))
 
     with open(parfilename, 'w') as parfile:
         parfile.write('*** Station source parameters ***\n')
@@ -59,15 +60,17 @@ def _write_parfile(config, evid, sourcepar):
             if statId in ['means', 'errors']:
                 continue
             par = sourcepar[statId]
-            parfile.write('%s\t' % statId)
+            parfile.write('{:>14} {}\t'.format(*statId.split()))
             for key in par:
                 if key in ['Mo', 'Er']:
-                    parfile.write('  %s %.3e ' % (key, par[key]))
+                    parfile.write('  {} {:.3e} '.format(key, par[key]))
+                elif key in ['hyp_dist', 'az']:
+                    parfile.write('  {} {:7.3f} '.format(key, par[key]))
                 else:
                     if par[key] is not None:
-                        parfile.write('  %s %6.3f ' % (key, par[key]))
+                        parfile.write('  {} {:6.3f} '.format(key, par[key]))
                     else:
-                        parfile.write('  %s %s ' % (key, par[key]))
+                        parfile.write('  {} {} '.format(key, par[key]))
             parfile.write('\n')
 
         means = sourcepar['means']
@@ -77,50 +80,50 @@ def _write_parfile(config, evid, sourcepar):
 
         Mw_mean = means['Mw']
         Mw_error = errors['Mw']
-        parfile.write('Mw: %.2f +/- %.2f\n' % (Mw_mean, Mw_error))
+        parfile.write('Mw: {:.2f} +/- {:.2f}\n'.format(Mw_mean, Mw_error))
 
         Mo_mean = means['Mo']
         Mo_minus, Mo_plus = errors['Mo']
         # format Mo_plus and Mo_minus to print it with the same exponent of Mo
         Mo_minus_str = _format_exponent(Mo_minus, Mo_mean)
         Mo_plus_str = _format_exponent(Mo_plus, Mo_mean)
-        parfile.write('Mo: %.3e /- %s /+ %s N.m\n' %
-                      (Mo_mean, Mo_minus_str, Mo_plus_str))
+        parfile.write('Mo: {:.3e} /- {} /+ {} N.m\n'.format(
+            Mo_mean, Mo_minus_str, Mo_plus_str))
 
         fc_mean = means['fc']
         fc_minus, fc_plus = errors['fc']
-        parfile.write('fc: %.3f /- %.3f /+ %.3f Hz\n' %
-                      (fc_mean, fc_minus, fc_plus))
+        parfile.write('fc: {:.3f} /- {:.3f} /+ {:.3f} Hz\n'.format(
+            fc_mean, fc_minus, fc_plus))
 
         t_star_mean = means['t_star']
         t_star_error = errors['t_star']
-        parfile.write('t_star: %.3f +/- %.3f Hz\n' %
-                      (t_star_mean, t_star_error))
+        parfile.write('t_star: {:.3f} +/- {:.3f} Hz\n'.format(
+            t_star_mean, t_star_error))
 
         ra_mean = means['ra']
         ra_minus, ra_plus = errors['ra']
-        parfile.write('Source radius: %.3f /- %.3f /+ %.3f m\n' %
-                      (ra_mean, ra_minus, ra_plus))
+        parfile.write('Source radius: {:.3f} /- {:.3f} /+ {:.3f} m\n'.format(
+            ra_mean, ra_minus, ra_plus))
 
         bsd_mean = means['bsd']
         bsd_error = errors['bsd']
         # format Mo_std to print it with the same exponent of Mo
         bsd_error_str = _format_exponent(bsd_error, bsd_mean)
-        parfile.write('Brune stress drop: %.3e +/- %s MPa\n' %
-                      (bsd_mean, bsd_error_str))
+        parfile.write('Brune stress drop: {:.3e} +/- {} MPa\n'.format(
+            bsd_mean, bsd_error_str))
 
         if means['Ml'] is not None:
             Ml_mean = means['Ml']
             Ml_error = errors['Ml']
-            parfile.write('Ml: %.3f +/- %.3f \n' % (Ml_mean, Ml_error))
+            parfile.write('Ml: {:.3f} +/- {:.3f} \n'.format(Ml_mean, Ml_error))
 
         Er_mean = means['Er']
         Er_minus, Er_plus = errors['Er']
         # format Er_plus and Er_minus to print it with the same exponent of Er
         Er_minus_str = _format_exponent(Er_minus, Er_mean)
         Er_plus_str = _format_exponent(Er_plus, Er_mean)
-        parfile.write('Er: %.3e /- %s /+ %s N.m\n' %
-                      (Er_mean, Er_minus_str, Er_plus_str))
+        parfile.write('Er: {:.3e} /- {} /+ {} N.m\n'.format(
+            Er_mean, Er_minus_str, Er_plus_str))
 
     logging.info('Output written to file: ' + parfilename)
 
@@ -187,9 +190,9 @@ def _write_hypo(config, evid, sourcepar):
         line = list(line)
 
     means = sourcepar['means']
-    mw_str = '%03.2f' % means['Mw']
+    mw_str = '{:03.2f}'.format(means['Mw'])
     if means['Ml'] is not None:
-        ml_str = '%03.2f' % means['Ml']
+        ml_str = '{:03.2f}'.format(means['Ml'])
     else:
         ml_str = ' '*4
     for i in range(0, 4):
@@ -197,7 +200,8 @@ def _write_hypo(config, evid, sourcepar):
         #line[45+i] = mw_str[0+i]
         line[69+i] = ml_str[0+i]
     outline = ''.join(line)
-    hypo_file_out = os.path.join(config.options.outdir, '%s.ssp.h' % evid)
+    hypo_file_out = os.path.join(
+        config.options.outdir, '{}.ssp.h'.format(evid))
     with open(hypo_file_out, 'w') as fp:
         try:
             fp.write(line1)
@@ -278,6 +282,6 @@ def write_output(config, evid, sourcepar, sourcepar_err):
     params_name = ('Mw', 'fc', 't_star')
     sourcepar_mean = dict(zip(params_name,
                               [means['Mw'], means['fc'], means['t_star']]))
-    logging.info('params_mean: %s' % (sourcepar_mean))
+    logging.info('params_mean: {}'.format(sourcepar_mean))
 
     return sourcepar_mean
