6c0e70825b260be7ed95caddc71e1b016a9185f9
Claudio Satriano <satriano@gmail.com>
2020-04-24 16:01:42 +0200

    Let AsyncPlotter.save() accept kwargs

--- sourcespec/AsyncPlotter.py
+++ sourcespec/AsyncPlotter.py
@@ -4,11 +4,11 @@ import multiprocessing as mp
 import time
 
 
-def _async_plotter(nc, fig, filename, bbox_inches, processes):
+def _async_plotter(nc, processes, fig, filename, **kwargs):
     while nc.value >= processes:
         time.sleep(0.1)
     nc.value += 1
-    fig.savefig(filename, bbox_inches=bbox_inches)
+    fig.savefig(filename, **kwargs)
     nc.value -= 1
 
 
@@ -19,10 +19,12 @@ class AsyncPlotter():
         self.pids = []
         self.processes = processes
 
-    def save(self, fig, filename, bbox_inches=None):
-        p = mp.Process(target=_async_plotter,
-                       args=(self.nc, fig, filename, bbox_inches,
-                             self.processes))
+    def save(self, fig, filename, **kwargs):
+        p = mp.Process(
+            target=_async_plotter,
+            args=(self.nc, self.processes, fig, filename),
+            kwargs=kwargs
+        )
         p.start()
         self.pids.append(p)
 
