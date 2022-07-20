4f1cd77e3e80a9c33d043672b2c2c0559323f822
Claudio Satriano <satriano@gmail.com>
2022-07-20 17:33:16 +0200

    Refactor ssp_radiated_energy.py

--- sourcespec/ssp_radiated_energy.py
+++ sourcespec/ssp_radiated_energy.py
@@ -18,53 +18,79 @@ Compute radiated energy from spectral integration.
 import numpy as np
 
 
+def _spectral_integral(spec, t_star, fmax):
+    """Compute spectral integral in eq. (3) from Lancieri et al. (2012)."""
+    deltaf = spec.stats.delta
+    freq = spec.get_freq()
+    # Data is in moment units. Let's put it back to displacement units,
+    # and derive it to velocity through multiplication by 2*pi*freq:
+    # (2. is the free-surface amplification factor)
+    data = (spec.data/spec.coeff) * (2*np.pi*freq)
+    # Correct data for attenuation:
+    data *= np.exp(np.pi * t_star * freq)
+    # Compute the energy integral, up to fmax:
+    if fmax is not None:
+        data[freq > fmax] = 0.
+    integral = np.sum((data**2) * deltaf)
+    return integral
+
+
+def _radiated_energy_coefficient(rho, vel):
+    """Compute coefficient in eq. (3) from Lancieri et al. (2012)."""
+    # From Lancieri et al. (2012), eq. (3), the correction term is:
+    #       8 * pi * r**2 * C**2 * rho * vs
+    # We do not multiply by r**2, since data is already distance-corrected.
+    # From Boatwright et al. (2002), eq. (2), C = <Fs>/(Fs * S),
+    # where <Fs> is the average radiation pattern in the focal sphere
+    # and Fs is the radiation pattern for the given angle between source
+    # and receiver. Here we put <Fs>/Fs = 1, meaning that we rely on the
+    # averaging between measurements at different stations, instead of
+    # precise measurements at a single station.
+    # S is the free-surface amplification factor, whihch we put = 2
+    coeff = 8 * np.pi * (1./2.)**2 * rho * vel
+    return coeff
+
+
+def _finite_bandwidth_correction(spec, fc, fmax):
+    """
+    Compute finite bandwidth correction.
+
+    Expressed as the ratio R between the estimated energy
+    and the true energy (Di Bona & Rovelli 1988)
+    """
+    if fmax is None:
+        fmax = spec.get_freq()[-1]
+    R = 2./np.pi * (np.arctan2(fmax, fc) - (fmax/fc)/(1+(fmax/fc)**2.))
+    return R
+
+
 def radiated_energy(config, spec_st, sourcepar):
     """Compute radiated energy, using eq. (3) in Lancieri et al. (2012)."""
-    # Loop on '??H' spectra
-    for spec in [spec for spec in spec_st if (spec.stats.channel[-1] == 'H')]:
-        statId = '%s %s' % (spec.id, spec.stats.instrtype)
+    # Select specids with channel code: '??H'
+    spec_ids = [spec.id for spec in spec_st if spec.id[-1] == 'H']
+    for spec_id in spec_ids:
+        spec = spec_st.select(id=spec_id)[0]
+
+        statId = '{} {}'.format(spec_id, spec.stats.instrtype)
         try:
             par = sourcepar.station_parameters[statId]
         except KeyError:
             continue
+
         t_star = par['t_star']
-        deltaf = spec.stats.delta
-        freq = spec.get_freq()
-        rho = config.rho
-        vs = config.hypo.vs * 1000.
-        # Data is in moment units. Let's put it back to displacement units,
-        # and derive it to velocity through multiplication by 2*pi*freq:
-        # (2. is the free-surface amplification factor)
-        data = (spec.data/spec.coeff) * (2*np.pi*freq)
-        # Correct data for attenuation:
-        data *= np.exp(np.pi * t_star * freq)
-        # Compute the energy integral, up to fmax:
-        fmax = config.max_freq_Er
-        if fmax is not None:
-            data[freq > fmax] = 0.
-        Er = np.sum((data**2) * deltaf)
-        # Apply corrections:
-        # From Lancieri et al. (2012), eq. (3), the correction term is:
-        #       8 * pi * r**2 * C**2 * rho * vs
-        # We do not multiply by r**2, since data is already distance-corrected.
-        # From Boatwright et al. (2002), eq. (2), C = <Fs>/(Fs * S),
-        # where <Fs> is the average radiation pattern in the focal sphere
-        # and Fs is the radiation pattern for the given angle between source
-        # and receiver. Here we put <Fs>/Fs = 1, meaning that we rely on the
-        # averaging between measurements at different stations, instead of
-        # precise measurements at a single station.
-        # S is the free-surface amplification factor, whihch we put = 2
-        Er *= 8 * np.pi * (1./2.)**2 * rho * vs
-
-        # Let's now compute finite bandwidth correction
-        # expressed as the ratio R between the estimated energy
-        # and the true energy (Di Bona & Rovelli 1988):
-        if fmax is None:
-            fmax = freq[-1]
         fc = par['fc']
-        R = 2./np.pi * (np.arctan2(fmax, fc) - (fmax/fc)/(1+(fmax/fc)**2.))
+        fmax = config.max_freq_Er
+        rho = config.rho
+        if config.wave_type == 'P':
+            vel = config.hypo.vp * 1000.
+        elif config.wave_type in ['S', 'SV', 'SH']:
+            vel = config.hypo.vs * 1000.
 
-        # And apply the correction
+        integral = _spectral_integral(spec, t_star, fmax)
+        coeff = _radiated_energy_coefficient(rho, vel)
+        Er = coeff * integral
+
+        R = _finite_bandwidth_correction(spec, fc, fmax)
         Er /= R
 
         # Store in the parameter dictionary
