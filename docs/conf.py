dd5abc23bf4b1dbdbd758afcf10118435acf6a47
Claudio Satriano <satriano@gmail.com>
2022-07-26 12:30:54 +0200

    Add favicon to HTML docs

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosectionlabel',
+    'sphinx-favicon',
 ]
 autosectionlabel_prefix_document = True
 
@@ -153,10 +154,19 @@ html_logo = '../logo/SourceSpec_logo_white.svg'
 # pixels large.
 #html_favicon = None
 
+# Favicon, using sphinx-favicon extension
+favicons = [
+    {
+        "rel": "icon",
+        "static-file": "SourceSpec_icon.svg",
+        "type": "image/svg+xml",
+    },
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
