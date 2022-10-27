c7e678e01745a92323edde68dc976265c6c883f6
Claudio Satriano <satriano@gmail.com>
2022-10-27 15:53:41 +0200

    ssp_correction: select spectra by id

--- sourcespec/ssp_correction.py
+++ sourcespec/ssp_correction.py
@@ -36,28 +36,29 @@ def station_correction(spec_st, config):
         logger.error(msg)
         ssp_exit(1)
 
-    for spec in [spec for spec in spec_st if (spec.stats.channel[-1] == 'H')]:
-        station = spec.stats.station
-        if station in set(x.stats.station for x in residual):
-            # apply correction
-            corr = residual.select(station=station)[0]
-            freq = spec.get_freq()
-            fmin = freq.min()
-            fmax = freq.max()
-            corr = corr.slice(fmin, fmax)
-            corr.data_mag = moment_to_mag(corr.data)
-            spec_corr = spec.copy()
-            # uncorrected spectrum will have component name 'h'
-            spec.stats.channel = spec.stats.channel[:-1] + 'h'
-            spec_corr.data_mag -= corr.data_mag
-            # interpolate the corrected data_mag to log frequencies
-            f = interp1d(freq, spec_corr.data_mag, fill_value='extrapolate')
-            spec_corr.data_log_mag = f(spec_corr.freq_log)
-            # convert mag to moment
-            spec_corr.data = mag_to_moment(spec_corr.data_mag)
-            spec_corr.data_log = mag_to_moment(spec_corr.data_log_mag)
-            spec_st.append(spec_corr)
-            logger.info(
-                '{} corrected, frequency range is: {:.2f} {:.2f} Hz'.format(
-                    spec_corr.id, fmin, fmax))
+    H_specs = [spec for spec in spec_st if (spec.stats.channel[-1] == 'H')]
+    for spec in H_specs:
+        try:
+            corr = residual.select(id=spec.id)[0]
+        except IndexError:
+            continue
+        freq = spec.get_freq()
+        fmin = freq.min()
+        fmax = freq.max()
+        corr = corr.slice(fmin, fmax)
+        corr.data_mag = moment_to_mag(corr.data)
+        spec_corr = spec.copy()
+        # uncorrected spectrum will have component name 'h'
+        spec.stats.channel = spec.stats.channel[:-1] + 'h'
+        spec_corr.data_mag -= corr.data_mag
+        # interpolate the corrected data_mag to log frequencies
+        f = interp1d(freq, spec_corr.data_mag, fill_value='extrapolate')
+        spec_corr.data_log_mag = f(spec_corr.freq_log)
+        # convert mag to moment
+        spec_corr.data = mag_to_moment(spec_corr.data_mag)
+        spec_corr.data_log = mag_to_moment(spec_corr.data_log_mag)
+        spec_st.append(spec_corr)
+        logger.info(
+            '{} corrected, frequency range is: {:.2f} {:.2f} Hz'.format(
+                spec_corr.id, fmin, fmax))
     return spec_st
