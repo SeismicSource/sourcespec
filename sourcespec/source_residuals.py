c105320f36a467c48169afda6dba3feebfa2e0be
Claudio Satriano <satriano@gmail.com>
2022-10-27 15:47:11 +0200

    Make `source_residuals` great again

--- sourcespec/source_residuals.py
+++ sourcespec/source_residuals.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 # SPDX-License-Identifier: CECILL-2.1
 """
-Compute station residuals from source_spec output.
+Compute mean station residuals from source_spec output.
 
 :copyright:
     2013-2014 Claudio Satriano <satriano@ipgp.fr>,
@@ -14,7 +14,6 @@ Compute station residuals from source_spec output.
 """
 import sys
 import os
-from glob import glob
 from collections import defaultdict
 import pickle
 from argparse import ArgumentParser
@@ -28,25 +27,41 @@ matplotlib.use('Agg')  # NOQA
 
 def main():
     parser = ArgumentParser(
-        description='Compute station residuals from source_spec output.')
+        description='Compute mean station residuals from source_spec output.')
     parser.add_argument(
-        '-m', '--min_spectra', dest='min_spectra', action='store',
+        '-m', '--min_spectra', dest='min_spectra', type=int, action='store',
         default='20', help='minimum number of spectra to '
         'compute residuals (default=20)', metavar='NUMBER')
     parser.add_argument(
         '-p', '--plot', dest='plot', action='store_true',
         default=False, help='save residuals plots to file')
     parser.add_argument(
-        'sspec_out_dir', help='directory containing source_spec output')
+        'residual_files_dir',
+        help='directory containing source_spec residual files '
+             'in pickle format. Residual files can be in subdirectories '
+             '(e.g., a subdirectory for each event).')
     args = parser.parse_args()
 
-    resdir = args.sspec_out_dir
     min_spectra = int(args.min_spectra)
+    resfiles_dir = args.residual_files_dir
     outdir = 'sspec_residuals'
 
+    if not os.path.exists(resfiles_dir):
+        print('Error: directory "{}" does not exist.'.format(resfiles_dir))
+        sys.exit(1)
+
+    resfiles = []
+    for root, dirs, files in os.walk(resfiles_dir):
+        for file in files:
+            if file.endswith('residuals.pickle'):
+                resfiles.append(os.path.join(root, file))
+    if not resfiles:
+        print('No residual file found in directory: {}'.format(resfiles_dir))
+        sys.exit()
+
     residual_dict = defaultdict(Stream)
-    for resfile in glob(os.path.join(resdir, '*-res*.pickle')):
-        print(resfile)
+    for resfile in resfiles:
+        print('Found residual file: {}'.format(resfile))
         with open(resfile, 'rb') as fp:
             residual_st = pickle.load(fp)
         for spec in residual_st:
@@ -59,7 +74,7 @@ def main():
     for stat_id in sorted(residual_dict.keys()):
         if len(residual_dict[stat_id]) < min_spectra:
             continue
-        print(stat_id)
+        print('Processing station: {}'.format(stat_id))
 
         res = residual_dict[stat_id]
 
@@ -69,9 +84,9 @@ def main():
         freq_max = max(freqs_max)
 
         spec_mean = Spectrum()
+        spec_mean.id = stat_id
         spec_mean.stats.begin = freq_min
         spec_mean.stats.delta = res[0].stats.delta
-        spec_mean.stats.station = res[0].stats.station
         spec_mean.data_mag = None
         for spec in res:
             spec_slice = spec.slice(freq_min, freq_max, pad=True,
@@ -91,8 +106,7 @@ def main():
 
         # plot traces
         if args.plot:
-            stnm = spec_mean.stats.station
-            figurefile = os.path.join(outdir, stnm + '-res.png')
+            figurefile = os.path.join(outdir, '{}-res.png'.format(stat_id))
             fig = plt.figure(dpi=160)
             for spec in res:
                 plt.semilogx(spec.get_freq(), spec.data_mag, 'b-')
@@ -100,10 +114,15 @@ def main():
             plt.xlabel('frequency (Hz)')
             plt.ylabel('residual amplitude (obs - synth) in magnitude units')
             plt.title(
-                'residuals : ' + stnm + ', ' + str(len(res)) + ' records')
+                'residuals: {} – {} records'.format(stat_id, len(res))
+            )
             fig.savefig(figurefile, bbox_inches='tight')
             plt.close()
+            print('Residual plot saved to: {}'.format(figurefile))
 
     # writes the mean residuals (the stations corrections)
-    with open(os.path.join(outdir, 'residual_mean.pickle'), 'wb') as fp:
+    res_mean_file = 'residual_mean.pickle'
+    res_mean_file = os.path.join(outdir, res_mean_file)
+    with open(res_mean_file, 'wb') as fp:
         pickle.dump(residual_mean, fp)
+    print('Mean station residuals saved to: {}'.format(res_mean_file))
