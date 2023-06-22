be679486ece4fb7761bad742dfeda9d346fa2c05
Kris Vanneste <kris.vanneste@oma.be>
2023-06-22 22:24:58 +0200

    Fixed bug related to inv_frequency weighting in _freq_ranges_for_Mw0_and_tstar0 function.

--- sourcespec/ssp_inversion.py
+++ sourcespec/ssp_inversion.py
@@ -153,8 +153,15 @@ def _freq_ranges_for_Mw0_and_tstar0(config, weight, freq_log, statId):
         # the closest index to f_weight:
         idx1 = np.where(freq_log <= config.f_weight)[0][-1]
     elif config.weighting == 'inv_frequency':
-        idx0 = 0
-        idx1 = np.where(weight < 0.7)[0][0]
+        weight_idxs = np.where(weight >= 0.7)[0]
+        try:
+            idx0 = weight_idxs[0]
+        except IndexError:
+            idx0 = 0
+        try:
+            idx1 = weight_idxs[-1]
+        except IndexError:
+            idx1 = len(weight) - 1
     else:
         idx0 = 0
         idx1 = len(weight) // 2
