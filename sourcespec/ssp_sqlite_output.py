45fb935e8b52054be4f858ef1ce5956edc088b7f
Claudio Satriano <satriano@gmail.com>
2023-06-29 19:16:14 +0200

    Move database table definitions to different file

--- sourcespec/ssp_sqlite_output.py
+++ sourcespec/ssp_sqlite_output.py
@@ -13,10 +13,11 @@ import os.path
 import logging
 import sqlite3
 from sourcespec.ssp_setup import ssp_exit
+from sourcespec.ssp_db_definitions import (
+    DB_VERSION,
+    STATIONS_TABLE, STATIONS_PRIMARY_KEYS, EVENTS_TABLE, EVENTS_PRIMARY_KEYS)
 from sourcespec._version import get_versions
 logger = logging.getLogger(__name__.split('.')[-1])
-# Current supported DB version
-DB_VERSION = 2
 
 
 def _db_file_exists(db_file):
@@ -105,44 +106,14 @@ def _create_stations_table(cursor, db_file):
     :param cursor: SQLite cursor
     :type cursor: sqlite3.Cursor
     """
-    sql_create_stations_table = """CREATE TABLE IF NOT EXISTS Stations (
-        stid TEXT,
-        evid TEXT,
-        runid TEXT,
-        Mo REAL,
-        Mo_err_minus REAL,
-        Mo_err_plus REAL,
-        Mo_is_outlier INT,
-        Mw REAL,
-        Mw_err_minus REAL,
-        Mw_err_plus REAL,
-        Mw_is_outlier INT,
-        fc REAL,
-        fc_err_minus REAL,
-        fc_err_plus REAL,
-        fc_is_outlier INT,
-        t_star REAL,
-        t_star_err_minus REAL,
-        t_star_err_plus REAL,
-        t_star_is_outlier INT,
-        Qo REAL,
-        Qo_err_minus REAL,
-        Qo_err_plus REAL,
-        Qo_is_outlier INT,
-        bsd REAL,
-        bsd_err_minus REAL,
-        bsd_err_plus REAL,
-        bsd_is_outlier INT,
-        ra REAL,
-        ra_err_minus REAL,
-        ra_err_plus REAL,
-        ra_is_outlier INT,
-        Er REAL,
-        Er_is_outlier INT,
-        dist REAL,
-        azimuth REAL,
-        PRIMARY KEY (stid, evid, runid)
-    );"""
+    sql_create_stations_table = (
+        'CREATE TABLE IF NOT EXISTS Stations ('
+        + '\n'.join(
+            [f'{key} {value},' for key, value in STATIONS_TABLE.items()]
+        )
+        + 'PRIMARY KEY (' + ', '.join(STATIONS_PRIMARY_KEYS) + ')'
+        + ');'
+    )
     try:
         cursor.execute(sql_create_stations_table)
     except Exception as db_err:
@@ -211,114 +182,14 @@ def _create_events_table(cursor, db_file):
     :param cursor: SQLite cursor
     :type cursor: sqlite3.Cursor
     """
-    sql_create_events_table = """CREATE TABLE IF NOT EXISTS Events (
-        /* Event info */
-            evid TEXT,
-            runid TEXT,
-            orig_time REAL,
-            lon REAL,
-            lat REAL,
-            depth REAL,
-        /* Statistical info */
-            nobs INTEGER,
-            nsigma REAL,
-            mid_pct REAL,
-            lower_pct REAL,
-            upper_pct REAL,
-        /* Seismic moment */
-            Mo_mean REAL,
-            Mo_mean_err_minus REAL,
-            Mo_mean_err_plus REAL,
-            Mo_wmean REAL,
-            Mo_wmean_err_minus REAL,
-            Mo_wmean_err_plus REAL,
-            Mo_pctl REAL,
-            Mo_pctl_err_minus REAL,
-            Mo_pctl_err_plus REAL,
-        /* Moment magnitude */
-            Mw_mean REAL,
-            Mw_mean_err_minus REAL,
-            Mw_mean_err_plus REAL,
-            Mw_wmean REAL,
-            Mw_wmean_err_minus REAL,
-            Mw_wmean_err_plus REAL,
-            Mw_pctl REAL,
-            Mw_pctl_err_minus REAL,
-            Mw_pctl_err_plus REAL,
-        /* Corner frequency */
-            fc_mean REAL,
-            fc_mean_err_minus REAL,
-            fc_mean_err_plus REAL,
-            fc_wmean REAL,
-            fc_wmean_err_minus REAL,
-            fc_wmean_err_plus REAL,
-            fc_pctl REAL,
-            fc_pctl_err_minus REAL,
-            fc_pctl_err_plus REAL,
-        /* t-star */
-            t_star_mean REAL,
-            t_star_mean_err_minus REAL,
-            t_star_mean_err_plus REAL,
-            t_star_wmean REAL,
-            t_star_wmean_err_minus REAL,
-            t_star_wmean_err_plus REAL,
-            t_star_pctl REAL,
-            t_star_pctl_err_minus REAL,
-            t_star_pctl_err_plus REAL,
-        /* Qo */
-            Qo_mean REAL,
-            Qo_mean_err_minus REAL,
-            Qo_mean_err_plus REAL,
-            Qo_wmean REAL,
-            Qo_wmean_err_minus REAL,
-            Qo_wmean_err_plus REAL,
-            Qo_pctl REAL,
-            Qo_pctl_err_minus REAL,
-            Qo_pctl_err_plus REAL,
-        /* Source radius */
-            ra_mean REAL,
-            ra_mean_err_minus REAL,
-            ra_mean_err_plus REAL,
-            ra_wmean REAL,
-            ra_wmean_err_minus REAL,
-            ra_wmean_err_plus REAL,
-            ra_pctl REAL,
-            ra_pctl_err_minus REAL,
-            ra_pctl_err_plus REAL,
-        /* Brune stress drop */
-            bsd_mean REAL,
-            bsd_mean_err_minus REAL,
-            bsd_mean_err_plus REAL,
-            bsd_wmean REAL,
-            bsd_wmean_err_minus REAL,
-            bsd_wmean_err_plus REAL,
-            bsd_pctl REAL,
-            bsd_pctl_err_minus REAL,
-            bsd_pctl_err_plus REAL,
-        /* Radiated energy */
-            Er_mean REAL,
-            Er_mean_err_minus REAL,
-            Er_mean_err_plus REAL,
-            Er_pctl REAL,
-            Er_pctl_err_minus REAL,
-            Er_pctl_err_plus REAL,
-        /* Local magnitude */
-            Ml_mean REAL,
-            Ml_mean_err_minus REAL,
-            Ml_mean_err_plus REAL,
-            Ml_pctl REAL,
-            Ml_pctl_err_minus REAL,
-            Ml_pctl_err_plus REAL,
-        /* Run info */
-            run_completed TEXT,
-            sourcespec_version TEXT,
-            author_name TEXT,
-            author_email TEXT,
-            agency_full_name TEXT,
-            agency_short_name TEXT,
-            agency_url TEXT,
-            PRIMARY KEY (evid, runid)
-    );"""
+    sql_create_events_table = (
+        'CREATE TABLE IF NOT EXISTS Events ('
+        + '\n'.join(
+            [f'{key} {value},' for key, value in EVENTS_TABLE.items()]
+        )
+        + 'PRIMARY KEY (' + ', '.join(EVENTS_PRIMARY_KEYS) + ')'
+        + ');'
+    )
     try:
         cursor.execute(sql_create_events_table)
     except Exception as db_err:
