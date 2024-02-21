30611d735620fe7203ea963beaaef7dd6464bebe
Kris Vanneste <kris.vanneste@oma.be>
2024-02-21 14:36:36 +0100

    Set `noise_pre_time` to noise window length if it is None

--- sourcespec/ssp_process_traces.py
+++ sourcespec/ssp_process_traces.py
@@ -310,8 +310,12 @@ def _define_signal_and_noise_windows(config, trace):
         win_length = win_length_p
     elif config.wave_type[0] == 'S':
         win_length = win_length_s
-    t1 = max(trace.stats.starttime,
-            p_arrival_time - config.noise_pre_time - config.signal_pre_time)
+    # use win_length if noise_pre_time is None
+    noise_pre_time = config.noise_pre_time or win_length
+    t1 = max(
+        trace.stats.starttime,
+        p_arrival_time - noise_pre_time - config.signal_pre_time
+    )
     t2 = t1 + win_length
     if t2 >= (p_arrival_time - config.signal_pre_time):
         logger.warning(f'{trace.id}: noise window ends after P-wave arrival')
