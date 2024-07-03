5b9257910e9df39bcb7d97543e6685f55a776e94
Claudio Satriano <satriano@gmail.com>
2024-07-03 12:25:23 +0200

    Move all config-related files to a "config" directory

---
+++ sourcespec2/config/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+# SPDX-License-Identifier: CECILL-2.1
+"""
+Configuration classes and functions for SourceSpec
+
+:copyright:
+    2013-2024 Claudio Satriano <satriano@ipgp.fr>
+:license:
+    CeCILL Free Software License Agreement v2.1
+    (http://www.cecill.info/licences.en.html)
+"""
+from .config import Config  # noqa
