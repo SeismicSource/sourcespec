1e0e76149c8fda9bcdc97259cebf90cb834da39b
Claudio Satriano <satriano@gmail.com>
2024-07-09 19:07:55 +0200

    Use the global `config` object in ssp_summary_statistics.py

--- sourcespec2/source_spec.py
+++ sourcespec2/source_spec.py
@@ -69,7 +69,7 @@ def main():
 
     # Compute summary statistics from station spectral parameters
     from .ssp_summary_statistics import compute_summary_statistics
-    compute_summary_statistics(config, sspec_output)
+    compute_summary_statistics(sspec_output)
 
     # Save output
     from .ssp_output import write_output, save_spectra
