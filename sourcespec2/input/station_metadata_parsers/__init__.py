f10b4b10fe03d1caaddd18b62fbd64ffc5eab67a
Claudio Satriano <satriano@gmail.com>
2024-07-19 15:54:20 +0200

    Move asdf inventory parsing to a separate module

---
+++ sourcespec2/input/station_metadata_parsers/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+# SPDX-License-Identifier: CECILL-2.1
+"""
+Station metadata parsers for SourceSpec.
+
+:copyright:
+    2013-2024 Claudio Satriano <satriano@ipgp.fr>
+:license:
+    CeCILL Free Software License Agreement v2.1
+    (http://www.cecill.info/licences.en.html)
+"""
+from .asdf_inventory import parse_asdf_inventory  # noqa
