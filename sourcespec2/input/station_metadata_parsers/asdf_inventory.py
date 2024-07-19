40d6c8814f996267f00554470e2be32661bd03c9
Claudio Satriano <satriano@gmail.com>
2024-07-19 15:54:20 +0200

    Move asdf inventory parsing to a separate module

---
+++ sourcespec2/input/station_metadata_parsers/asdf_inventory.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: CECILL-2.1
+"""
+Read station metadata from ASDF files.
+
+:copyright:
+    2012-2024 Claudio Satriano <satriano@ipgp.fr>
+:license:
+    CeCILL Free Software License Agreement v2.1
+    (http://www.cecill.info/licences.en.html)
+"""
+import logging
+from obspy.core.inventory import Inventory
+from ...setup import ssp_exit
+logger = logging.getLogger(__name__.rsplit('.', maxsplit=1)[-1])
+
+
+def parse_asdf_inventory(asdf_file):
+    """
+    Read station metadata from ASDF file
+
+    :param asdf_file: full path to ASDF file
+    :type asdf_file: str
+
+    :return: inventory
+    :rtype: :class:`~obspy.core.inventory.inventory.Inventory`
+    """
+    try:
+        # pylint: disable=import-outside-toplevel
+        # pyasdf is not a hard dependency, so we import it here
+        # and check for ImportError
+        import pyasdf
+    except ImportError:
+        logger.error(
+            'Error importing pyasdf. '
+            'See https://seismicdata.github.io/pyasdf/ for installation '
+            'instructions.'
+        )
+        ssp_exit(1)
+    inventory = Inventory()
+    try:
+        ds = pyasdf.ASDFDataSet(asdf_file, mode='r')
+    except OSError:
+        logger.warning(f'Unable to read ASDF file: {asdf_file}')
+        return inventory
+    for nw_stat_code in ds.waveforms.list():
+        if 'StationXML' in ds.waveforms[nw_stat_code]:
+            station_inv = ds.waveforms[nw_stat_code].StationXML
+            inventory += station_inv
+    ds._close()
+    return inventory
