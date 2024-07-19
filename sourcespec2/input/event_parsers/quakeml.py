5bb72be3b3ca1943b3b57027ba1c01af100b9796
Claudio Satriano <satriano@gmail.com>
2024-07-19 12:30:45 +0200

    Make parse_qml_event_picks() behave like parse_asdf_event_picks()
    The function was previously named parse_qml_file()

--- sourcespec2/input/event_parsers/quakeml.py
+++ sourcespec2/input/event_parsers/quakeml.py
@@ -11,26 +11,30 @@ Parse event metadata and picks from a QuakeML file.
 """
 import logging
 from obspy import read_events
-from ...setup import config, ssp_exit
+from ...setup import ssp_exit
 from .obspy_catalog import parse_obspy_catalog
 logger = logging.getLogger(__name__.rsplit('.', maxsplit=1)[-1])
 
 
-def parse_qml_file():
+def parse_qml_event_picks(qml_file, event_id=None):
     """
     Parse event metadata and picks from a QuakeML file.
+
+    :param qml_file: Path to the QuakeML file.
+    :type qml_file: str
+    :param event_id: event id
+    :type event_id: str
 
     :return: a tuple of (SSPEvent, picks)
     :rtype: tuple
     """
-    qml_file = config.options.qml_file
     if qml_file is None:
         ssp_event = None
         picks = []
         return ssp_event, picks
     try:
         obspy_catalog = read_events(qml_file)
-        return parse_obspy_catalog(obspy_catalog, file_name=qml_file)
+        return parse_obspy_catalog(obspy_catalog, event_id, qml_file)
     except Exception as err:
         logger.error(err)
         ssp_exit(1)
