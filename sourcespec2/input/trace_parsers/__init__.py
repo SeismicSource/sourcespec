5d06b6c7e5f917311a1f95a02d1aadec5a7f424b
Claudio Satriano <satriano@gmail.com>
2024-07-19 15:16:44 +0200

    Move asdf trace parsing to a separate module

---
+++ sourcespec2/input/trace_parsers/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+# SPDX-License-Identifier: CECILL-2.1
+"""
+Trace parsers for SourceSpec.
+
+:copyright:
+    2013-2024 Claudio Satriano <satriano@ipgp.fr>
+:license:
+    CeCILL Free Software License Agreement v2.1
+    (http://www.cecill.info/licences.en.html)
+"""
+from .asdf_traces import parse_asdf_traces  # noqa
