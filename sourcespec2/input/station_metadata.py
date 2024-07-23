454275173c94b22abcabb5d3570313315e4411ca
Claudio Satriano <satriano@gmail.com>
2024-07-23 12:43:44 +0200

    Move instrument metadata parsing from SAC header to `read_traces` and `read_station_metadata`.

--- sourcespec2/input/station_metadata.py
+++ sourcespec2/input/station_metadata.py
@@ -12,11 +12,160 @@ PAZ (SAC polezero format), ASDF
 """
 import os
 import logging
+import contextlib
 from obspy import read_inventory
 from obspy.core.inventory import Inventory
 from ..setup import config
-from .station_metadata_parsers import read_paz_file, parse_asdf_inventory
+from .station_metadata_parsers import (
+    PAZ,
+    read_paz_file,
+    parse_asdf_inventory,
+    is_SAC_trace,
+    compute_sensitivity_from_SAC,
+    get_station_coordinates_from_SAC
+)
 logger = logging.getLogger(__name__.rsplit('.', maxsplit=1)[-1])
+
+
+def _update_coords(obj, coords):
+    """
+    Update coordinates the given object.
+
+    :param obj: ObsPy Station or Channel object
+    :type obj: :class:`obspy.core.inventory.station.Station`
+        or :class:`obspy.core.inventory.channel.Channel`
+    :param coords: Dictionary containing coordinates
+    :type coords: dict
+    """
+    obj.latitude = coords['latitude']
+    obj.longitude = coords['longitude']
+    obj.elevation = coords['elevation']
+    with contextlib.suppress(KeyError):
+        obj.depth = coords['local_depth']
+
+
+def _update_inventory_with_trace_coords(trace, inventory):
+    """
+    Update inventory with station coordinates from a trace.
+
+    The inventory is updated in place.
+
+    :param trace: ObsPy trace object
+    :type trace: :class:`obspy.core.trace.Trace`
+    :param inventory: ObsPy Inventory object to update
+    :type inventory: :class:`obspy.core
+
+    .. note::
+        Currently only SAC files are supported.
+    """
+    if not is_SAC_trace(trace):
+        return
+    # get coordinates from SAC headers
+    sac_coords = get_station_coordinates_from_SAC(trace)
+    if sac_coords is None:
+        return
+    for net in inventory:
+        if net.code != trace.stats.network:
+            continue
+        for sta in net:
+            if sta.code != trace.stats.station:
+                continue
+            for chan in sta:
+                if chan.location_code != trace.stats.location:
+                    continue
+                if chan.code != trace.stats.channel:
+                    continue
+                _update_coords(chan, sac_coords)
+            _update_coords(sta, sac_coords)
+
+
+def _update_inventory_with_trace_sensitivity(trace, inventory):
+    """
+    Update inventory with sensitivity from a trace.
+
+    :param trace: ObsPy trace object
+    :type trace: :class:`obspy.core.trace.Trace`
+    :param inventory: ObsPy Inventory object to update
+    :type inventory: :class:`obspy.core.inventory.inventory.Inventory`
+
+    :return: inventory
+    :rtype: :class:`~obspy.core.inventory.inventory.Inventory`
+
+    .. note::
+        Currently only SAC files are supported.
+    """
+    if not is_SAC_trace(trace):
+        return inventory
+    update_inventory = inventory.copy()
+    trace_inv = update_inventory.select(
+        network=trace.stats.network,
+        station=trace.stats.station,
+        location=trace.stats.location,
+        channel=trace.stats.channel,
+        starttime=trace.stats.starttime
+    )
+    # save coordinates from the inventory, if available
+    coords = None
+    with contextlib.suppress(Exception):
+        coords = trace_inv.get_coordinates(
+            trace.id, trace.stats.starttime)
+    paz = PAZ()
+    paz.seedID = trace.id
+    paz.sensitivity = compute_sensitivity_from_SAC(trace)
+    paz.poles = []
+    paz.zeros = []
+    if trace_inv:
+        logger.warning(
+            f'Overriding response for {trace.id} with constant '
+            f'sensitivity {paz.sensitivity}')
+    # override the trace_inv object with a new one constructed from the
+    # sensitivity value
+    trace_inv = paz.to_inventory()
+    # restore coordinates, if available
+    if coords is not None:
+        for sta in trace_inv[0].stations:
+            _update_coords(sta, coords)
+            for chan in sta.channels:
+                _update_coords(chan, coords)
+    else:
+        _update_inventory_with_trace_coords(trace, trace_inv)
+    # update inventory
+    update_inventory = update_inventory.remove(
+        network=trace.stats.network,
+        station=trace.stats.station,
+        location=trace.stats.location,
+        channel=trace.stats.channel
+    ) + trace_inv
+    return update_inventory
+
+
+def _update_inventory_from_stream(stream, inventory):
+    """
+    Update inventory with station metadata from a stream.
+
+    :param stream: ObsPy Stream object containing station metadata
+    :type stream: :class:`obspy.core.stream.Stream`
+    :param inventory: ObsPy Inventory object to update
+    :type inventory: :class:`obspy.core.inventory.inventory.Inventory`
+
+    :return: inventory
+    :rtype: :class:`~obspy.core.inventory.inventory.Inventory`
+
+    .. note::
+        Currently only SAC files are supported.
+    """
+    update_inventory = inventory.copy()
+    for trace in stream:
+        # ignore traces that are not in SAC format
+        if not hasattr(trace.stats, 'sac'):
+            continue
+        _update_inventory_with_trace_coords(trace, update_inventory)
+        # If a "sensitivity" config option is provided, override the Inventory
+        # object with a new one constructed from the sensitivity value
+        if config.sensitivity is not None:
+            update_inventory = _update_inventory_with_trace_sensitivity(
+                trace, update_inventory)
+    return update_inventory
 
 
 def _read_asdf_inventory():
@@ -78,15 +227,20 @@ def _read_station_metadata_from_files():
     return inventory
 
 
-def read_station_metadata():
+def read_station_metadata(stream=None):
     """
     Read station metadata.
+
+    :param stream: ObsPy Stream object containing station metadata (optional)
+    :type stream: :class:`obspy.core.stream.Stream`
 
     :return: inventory
     :rtype: :class:`~obspy.core.inventory.inventory.Inventory`
     """
     logger.info('Reading station metadata...')
     inventory = _read_station_metadata_from_files() + _read_asdf_inventory()
+    if stream is not None:
+        inventory = _update_inventory_from_stream(stream, inventory)
     nstations = len(inventory.get_contents()['stations'])
     logger.info(f'Reading station metadata: {nstations} stations read')
     logger.info('---------------------------------------------------')
