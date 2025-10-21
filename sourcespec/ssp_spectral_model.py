caac8b2c27f0fa71cdd153db94ba9d6727ab3280
Claudio Satriano <satriano@gmail.com>
2025-10-21 13:45:41 +0200

    Spectral model: allow passing t_star as a function of frequency
    Also update docstrings for clarity

--- sourcespec/ssp_spectral_model.py
+++ sourcespec/ssp_spectral_model.py
@@ -21,25 +21,52 @@ import numpy as np
 
 def spectral_model(freq, Mw, fc, t_star, alpha=1.):
     r"""
-    Spectral model.
+    Spectral model for seismic source spectrum.
+
+    This function computes the theoretical spectral model based on the Brune
+    source model with attenuation, expressed in terms of moment magnitude.
 
     .. math::
 
-       Y_{data} = M_w + \frac{2}{3} \left[ - \log_{10} \left(
-                        1+\left(\frac{f}{f_c}\right)^2 \right) -
-                        \pi \, f t^* \log_{10} e \right]
+        Y_{data} = M_w + \frac{2}{3} \left[ - \log_{10} \left(
+                              1+\left(\frac{f}{f_c}\right)^2 \right) -
+                              \pi \, f^\alpha t^*(f) \log_{10} e \right]
 
-    see :ref:`Theoretical Background <theoretical_background>`
-    for a detailed derivation of this model.
+    See :ref:`Theoretical Background <theoretical_background>`
+
+    Parameters
+    ----------
+    freq : float or array-like
+         Frequency or array of frequencies (in Hz) at which to compute the
+         spectral model.
+    Mw : float
+         Moment magnitude of the seismic event.
+    fc : float
+         Corner frequency (in Hz) of the source spectrum.
+    t_star : float or callable
+         Attenuation parameter t* (in seconds), equal to travel time divided
+         by quality factor (tt/Q). Can be a constant value or a function of
+         frequency.
+    alpha : float, optional
+         Frequency exponent for the attenuation term. Default is 1.0 for
+         standard attenuation model.
+
+    Returns
+    -------
+    float or array-like
+         Logarithmic spectral amplitude(s) at the specified
+         frequency/frequencies.
     """
     # log S(w)= log(coeff*Mo) + log((1/(1+(w/wc)^2)) + \
     #           log (exp (- w *t_star/2))
     # attenuation model: exp[-pi t* f] with t*=T /Q
     loge = math.log10(math.e)
+    # Check if t_star is callable (function) or a constant
+    t_star_val = t_star(freq) if callable(t_star) else t_star
     return (
         Mw -
         (2. / 3.) * np.log10(1. + np.power((freq / fc), 2)) -
-        (2. / 3.) * loge * (math.pi * np.power(freq, alpha) * t_star)
+        (2. / 3.) * loge * (math.pi * np.power(freq, alpha) * t_star_val)
     )
 
 
@@ -47,10 +74,19 @@ def objective_func(xdata, ydata, weight):
     """
     Objective function generator for bounded inversion.
 
-    :param xdata: x data (frequencies)
-    :param ydata: y data (log spectral amplitudes)
-    :param weight: weights for each data point
-    :return: objective function
+    Parameters
+    ----------
+    xdata : array-like
+        x data (frequencies)
+    ydata : array-like
+        y data (log spectral amplitudes)
+    weight : array-like
+        weights for each data point
+
+    Returns
+    -------
+    callable
+        objective function
     """
     errsum = np.sum(weight)
 
@@ -58,8 +94,15 @@ def objective_func(xdata, ydata, weight):
         """
         Objective function for bounded inversion.
 
-        :param params: model parameters (Mw, fc, t_star, [alpha])
-        :return: RMS misfit
+        Parameters
+        ----------
+        params : array-like
+            Model parameters (Mw, fc, t_star, [alpha])
+
+        Returns
+        -------
+        float
+            RMS misfit
         """
         # params components should be np.float
         if len(params) == 4:
